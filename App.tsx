19f3e579419d633de971332bee0764b6377bd0fa
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-02-27 19:03:58 -0300

    chore: setup onesign notification request and update scripts

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import { Loading } from './src/components/Loading';
 import { CartContextProvider } from './src/contexts/CartContext';
 
 OneSignal.initialize('66a74ab6-34fb-45db-bf00-114f6c42b4f1')
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
